ed3500d74e4980ad0c515f508ec81cd1cd18f60c
tolia k <tolia.kohan@gmail.com>
2025-05-02 10:00:47 +0300

    add requests for reviews

--- src/services/backend/reviewsRequests.js
+++ src/services/backend/reviewsRequests.js
@@ -8,3 +8,34 @@ export async function reviewsIndexRequest(authorization) {
     })
 }
 
+export async function reviewsCreateRequest(authorization, data) {
+    return await instance.post("reviews/", data, {
+        headers: {
+            Authorization: authorization
+        }
+    })
+}
+
+export async function reviewsUpdateRequest(authorization, id, data) {
+    return await instance.patch(`reviews/${id}/`, data, {
+        headers: {
+            Authorization: authorization
+        }
+    })
+}
+
+export async function reviewsDeleteRequest(authorization, id) {
+    return await instance.delete(`reviews/${id}/`, {
+        headers: {
+            Authorization: authorization
+        }
+    })
+}
+
+export async function reviewShowRequest(authorization, id) {
+    return await instance.get(`reviews/${id}/`, {
+        headers: {
+            Authorization: authorization
+        }
+    })
+}
