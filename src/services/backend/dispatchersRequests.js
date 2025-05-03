c0b965c700377848d26eba969ce1cc66854fdf47
tolia k <tolia.kohan@gmail.com>
2025-05-03 19:23:58 +0300

    add requests for dispatchers

--- src/services/backend/dispatchersRequests.js
+++ src/services/backend/dispatchersRequests.js
@@ -7,3 +7,35 @@ export async function dispatchersCreateRequest(data, authorization) {
         }
     })
 }
+
+export async function dispatchersIndexRequest(authorization) {
+    return await instance.get("/dispatchers/", {
+        headers: {
+            Authorization: authorization
+        }
+    })
+}
+
+export async function dispatchersShowRequest(id, authorization) {
+    return await instance.get(`/dispatchers/${id}/`, {
+        headers: {
+            Authorization: authorization
+        }
+    })
+}
+
+export async function dispatchersUpdateRequest(id, data, authorization) {
+    return await instance.patch(`/dispatchers/${id}/`, data, {
+        headers: {
+            Authorization: authorization
+        }
+    })
+}
+
+export async function dispatchersDeleteRequest(id, authorization) {
+    return await instance.delete(`/dispatchers/${id}/`, {
+        headers: {
+            Authorization: authorization
+        }
+    })
+}
