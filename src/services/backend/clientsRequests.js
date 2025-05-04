75c1229028502a97793b36f4f1c8bdc3d0b68e50
tolia k <tolia.kohan@gmail.com>
2025-05-04 13:02:34 +0300

    add client requests

--- src/services/backend/clientsRequests.js
+++ src/services/backend/clientsRequests.js
@@ -16,3 +16,18 @@ export async function clientShowRequest(id, authorization) {
     })
 }
 
+export async function clientDeleteRequest(id, authorization) {
+    return await instance.delete(`/clients/${id}`, {
+        headers: {
+            Authorization: authorization,
+        }
+    })
+}
+
+export async function clientUpdateRequest(id, data, authorization) {
+    return await instance.patch(`/clients/${id}`, data, {
+        headers: {
+            Authorization: authorization,
+        }
+    })
+}
